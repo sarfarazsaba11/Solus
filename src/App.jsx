19bc652eb8e78695ae09ce382f33660815e9361e
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-07-21 23:08:16 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Footer from "./components/footer"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="max-w-310 mx-auto">
+
+    <Navbar/>
+    <Hero/>
+    <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default App
