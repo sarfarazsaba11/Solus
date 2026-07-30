33402ed4847be1e32c4d0160373540671ed5438b
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-07-30 20:29:46 +0500

    Add Today's Menu section to display the menu component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Footer from "./components/footer"
 
 import './App.css'
+import { Menu } from "./components/Restaurant/Menu"
 
 function App() {
 
@@ -10,10 +11,15 @@ function App() {
   return (
     <>
     <div className="max-w-310 mx-auto">
+      <div className="relative">
+          <h1 className="relative mt-20 font-bold text-5xl text-center">
+          Today's Menu
+          </h1>
 
-    <Navbar/>
-    <Hero/>
-    <Footer/>
+          <div className="absolute -bottom-6 left-[35%] z-10 h-2 w-100 bg-amber-400 " />
+</div>
+
+    <Menu/>
     </div>
     </>
   )
