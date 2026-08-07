3543ae88996ce41798d96665f21a6e15d30cdebe
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-08-07 23:24:31 +0500

    Refactor Menu component for improved readability and structure

--- src/components/Restaurant/Menu.jsx
+++ src/components/Restaurant/Menu.jsx
@@ -1,16 +1,31 @@
-import {menu} from '../../lib/data'
-import { MenuItem } from './MenuItem'
+import { menu } from "../../lib/data";
+import { MenuItem } from "./MenuItem";
 
+export function Menu() {
+  const visibleMenu = menu.filter((item) => item.inStock);
 
-export function Menu (){
+  if (visibleMenu.length === 0) {
+    return (
+      <p className="text-center text-slate-500 p-12">
+        restaurant is closed today.
+      </p>
+    );
+  }
 
-    const visibleMenu = menu.filter(item => item.inStock)
+  return (
+    <div>
+      <div className="relative">
+        <h1 className="relative mt-20 font-bold text-5xl text-center">
+          Today's Menu
+        </h1>
 
-
-    return(
-        <div className='grid grid-cols-1  sm:grid-cols-2 gap-4 lg:grid-cols-3 mt-16 '>
-            {visibleMenu.map((item)=><MenuItem key={item.id} item={item}/> )}
-        </div>
-
-    )
+        <div className="absolute -bottom-6 left-[35%] z-10 h-2 w-100 bg-amber-400 " />
+      </div>
+      <div className="grid grid-cols-1  sm:grid-cols-2 gap-4 lg:grid-cols-3 mt-16 ">
+        {visibleMenu.map((item) => (
+          <MenuItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
