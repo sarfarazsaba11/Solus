083b11c87e322ec1f9e97b5408065fbe36ad9ccb
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-08-13 23:45:35 +0500

    Replace Menu component with MenuPage for updated menu display

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Hero from "./components/Hero"
 import Footer from "./components/footer"
 
 import './App.css'
-import { Menu } from "./components/Restaurant/Menu"
+import MenuPage from "./components/challenge/Menupage"
+
 
 function App() {
 
@@ -11,15 +12,10 @@ function App() {
   return (
     <>
     <div className="max-w-310 mx-auto">
-      <div className="relative">
-          <h1 className="relative mt-20 font-bold text-5xl text-center">
-          Today's Menu
-          </h1>
+   
 
-          <div className="absolute -bottom-6 left-[35%] z-10 h-2 w-100 bg-amber-400 " />
-</div>
-
-    <Menu/>
+    <MenuPage/>
+   
     </div>
     </>
   )
