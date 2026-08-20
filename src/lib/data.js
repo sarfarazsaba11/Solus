0d3d63e916c939552f62901fb83b7bf434e72fcf
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-08-20 23:48:21 +0500

    Make lib folder create data file in it ,

---
+++ src/lib/data.js
@@ -0,0 +1,59 @@
+
+
+
+export const menu = [
+  {
+    id: "pizza",
+    name: "Pizza",
+    price: 24.99,
+    imageSrc: "/images/pizza.jpg",
+    inStock: true,
+    category: "Italian",
+    soldOut:true
+  },
+  {
+    id: "biryani",
+    name: "Chicken Biryani",
+    price: 18.99,
+    imageSrc:"/images/biryani.jpg",
+    inStock: true,
+    category: "Indian",
+    soldOut:false
+  },
+  {
+    id: "sushi",
+    name: "Sushi",
+    price: 32.5,
+    imageSrc: "/images/sushi.jpg",
+    inStock: false,
+    category: "Japanese",
+    soldOut:false
+  },
+  {
+    id: "burger",
+    name: "Burger",
+    price: 15.99,
+    imageSrc: "/images/burger.jpg",
+    inStock: true,
+    category: "Fast Food",
+    soldOut:true
+  },
+  {
+    id: "seekh-kabab",
+    name: "Seekh kabab",
+    price: 19.5,
+    imageSrc: "/images/kabab.jpg",
+    inStock: true,
+    category: "Mexican",
+    soldOut:false
+  },
+  {
+    id: 6,
+    name: "kheer",
+    price: 27.99,
+    imageSrc: "/images/kheer.jpg",
+    inStock: false,
+    category: "Indian",
+    soldOut:true
+  },
+];
